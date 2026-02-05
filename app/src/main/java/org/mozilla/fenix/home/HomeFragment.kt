3a0e7979d0d95d9611d25e63c748da3a12060ec9
Serban Stanca <sstanca@mozilla.com>
2026-02-05 12:55:05 +0200

    Revert "Bug 1985031 - Detect and handle "Don't ask again" state for Default Browser prompt r=android-reviewers,twhite,rebecatudor273" for causing fenix-debug failures in DefaultSessionControlControllerTest.kt.
    This reverts commit bd017d4c733c2ce819c6e82981b4154521893c1d.

--- app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
+++ app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
@@ -59,8 +59,6 @@ import mozilla.components.concept.sync.AccountObserver
 import mozilla.components.concept.sync.AuthType
 import mozilla.components.concept.sync.OAuthAccount
 import mozilla.components.feature.accounts.push.SendTabUseCases
-import mozilla.components.feature.downloads.DateTimeProvider
-import mozilla.components.feature.downloads.DefaultDateTimeProvider
 import mozilla.components.feature.tab.collections.TabCollection
 import mozilla.components.feature.top.sites.presenter.DefaultTopSitesPresenter
 import mozilla.components.lib.state.ext.consumeFlow
@@ -68,9 +66,7 @@ import mozilla.components.lib.state.ext.consumeFrom
 import mozilla.components.lib.state.ext.flow
 import mozilla.components.lib.state.ext.observeAsComposableState
 import mozilla.components.support.base.feature.ViewBoundFeatureWrapper
-import mozilla.components.support.utils.BuildManufacturerChecker
 import mozilla.components.support.utils.KeyboardState
-import mozilla.components.support.utils.ext.navigateToDefaultBrowserAppsSettings
 import mozilla.components.support.utils.keyboardAsState
 import mozilla.telemetry.glean.private.NoExtras
 import org.mozilla.fenix.BrowserDirection
@@ -235,7 +231,6 @@ class HomeFragment : Fragment() {
             settings = requireComponents.settings,
         )
     }
-    private val dateTimeProvider: DateTimeProvider by lazy { DefaultDateTimeProvider() }
 
     private lateinit var privacyNoticeBannerStore: PrivacyNoticeBannerStore
 
@@ -291,19 +286,6 @@ class HomeFragment : Fragment() {
         NavController.OnDestinationChangedListener { _, destination, _ ->
             if (destination.id != R.id.homeFragment) {
                 privacyNoticeBannerStore.dispatch(PrivacyNoticeBannerAction.OnNavigatedAwayFromHome)
-            }
-        }
-
-    private val setToDefaultPromptRequestLauncher: ActivityResultLauncher<Intent> =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            with(requireContext()) {
-                maybeNavigateToSystemSetToDefaultAction(
-                    result.resultCode,
-                    settings(),
-                    dateTimeProvider,
-                ) {
-                    navigateToDefaultBrowserAppsSettings(BuildManufacturerChecker())
-                }
             }
         }
 
@@ -534,12 +516,6 @@ class HomeFragment : Fragment() {
             navControllerRef = WeakReference(findNavController()),
             viewLifecycleScope = viewLifecycleOwner.lifecycleScope,
             showAddSearchWidgetPrompt = ::showAddSearchWidgetPrompt,
-            requestSetDefaultBrowserPrompt = {
-                maybeRequestDefaultBrowserPrompt(
-                    WeakReference(activity),
-                    setToDefaultPromptRequestLauncher,
-                )
-            },
         ).apply {
             registerCallback(
                 object : SessionControlControllerCallback {
